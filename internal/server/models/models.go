b66e5c4f38adf80e8efa36e4c81ef461560c12db
Evgeny Geyer <72138233+eutjeng@users.noreply.github.com>
2023-09-29 15:02:55 +0300

    optimize memory usage by reordering struct fields

--- internal/server/models/models.go
+++ internal/server/models/models.go
@@ -1,8 +1,8 @@
 package models
 
 type Metrics struct {
-	ID    string   `json:"id"`              // metric name
-	MType string   `json:"type"`            // parameter that takes the value 'gauge' or 'counter'
 	Value *float64 `json:"value,omitempty"` // metric value when type is 'gauge'
 	Delta *int64   `json:"delta,omitempty"` // metric value when type is 'counter'
+	ID    string   `json:"id"`              // metric name
+	MType string   `json:"type"`            // parameter that takes the value 'gauge' or 'counter'
 }
