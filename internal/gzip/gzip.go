1c996b3a4a418121ea3ba8ac6aa12d684713e2f7
Evgeny Geyer <72138233+eutjeng@users.noreply.github.com>
2023-09-30 18:48:19 +0300

    create comments for gzip methods

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -9,20 +9,22 @@ import (
 	"go.uber.org/zap"
 )
 
-// compressWriter реализует интерфейс http.ResponseWriter и позволяет прозрачно для сервера
-// сжимать передаваемые данные и выставлять правильные HTTP-заголовки
+// compressWriter implements http.ResponseWriter and provides transparent
+// compression for server responses, setting appropriate HTTP headers
 type compressWriter struct {
-	w  http.ResponseWriter
-	zw *gzip.Writer
+	w          http.ResponseWriter // the original http.ResponseWriter
+	zw         *gzip.Writer        // gzip writer to compress the data
+	statusCode int                 // HTTP status code to set when writing the header
 }
 
-// compressReader реализует интерфейс io.ReadCloser и позволяет прозрачно для сервера
-// декомпрессировать получаемые от клиента данные
+// compressReader implements io.ReadCloser and provides transparent
+// decompression for incoming client data
 type compressReader struct {
-	r  io.ReadCloser
-	zr *gzip.Reader
+	r  io.ReadCloser // the original ReadCloser
+	zr *gzip.Reader  // gzip reader to decompress the data
 }
 
+// newCompressWriter creates a new compressWriter given an http.ResponseWriter
 func newCompressWriter(w http.ResponseWriter) *compressWriter {
 	return &compressWriter{
 		w:  w,
@@ -30,24 +32,34 @@ func newCompressWriter(w http.ResponseWriter) *compressWriter {
 	}
 }
 
+// Header returns the headers from the original http.ResponseWriter
 func (c *compressWriter) Header() http.Header {
 	return c.w.Header()
 }
 
+// Write writes the data, compressing it if necessary
 func (c *compressWriter) Write(p []byte) (int, error) {
-	return c.zw.Write(p)
+	if c.statusCode >= http.StatusOK && c.statusCode < http.StatusMultipleChoices {
+		c.w.Header().Set("Content-Encoding", "gzip")
+		c.w.WriteHeader(c.statusCode)
+		return c.zw.Write(p)
+	}
+
+	c.w.WriteHeader(c.statusCode)
+	return c.w.Write(p)
 }
 
+// WriteHeader sets the HTTP status code for the response
 func (c *compressWriter) WriteHeader(statusCode int) {
-	c.w.Header().Set("Content-Encoding", "gzip")
-	c.w.WriteHeader(statusCode)
+	c.statusCode = statusCode
 }
 
-// Close закрывает gzip.Writer и досылает все данные из буфера.
+// Close closes the gzip.Writer and flushes any buffered data
 func (c *compressWriter) Close() error {
 	return c.zw.Close()
 }
 
+// newCompressReader creates a new compressReader given an io.ReadCloser
 func newCompressReader(r io.ReadCloser) (*compressReader, error) {
 	zr, err := gzip.NewReader(r)
 	if err != nil {
@@ -60,10 +72,12 @@ func newCompressReader(r io.ReadCloser) (*compressReader, error) {
 	}, nil
 }
 
+// Read reads from the decompressed data stream
 func (c compressReader) Read(p []byte) (n int, err error) {
 	return c.zr.Read(p)
 }
 
+// Close closes both the original ReadCloser and the gzip.Reader
 func (c *compressReader) Close() error {
 	if err := c.r.Close(); err != nil {
 		return err
@@ -71,28 +85,33 @@ func (c *compressReader) Close() error {
 	return c.zr.Close()
 }
 
+// WithCompression returns a middleware that adds gzip compression and
+// decompression to the request/response cycle
 func WithCompression(sugar *zap.SugaredLogger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// оригинальный http.ResponseWriter
 			ow := w
 
-			// проверка поддержки gzip клиентом
 			acceptEncoding := r.Header.Get("Accept-Encoding")
 			supportsGzip := strings.Contains(acceptEncoding, "gzip")
+			var cw *compressWriter
 			if supportsGzip {
-				// обертка для сжатия
-				cw := newCompressWriter(w)
+				cw = newCompressWriter(w)
 				ow = cw
-				defer cw.Close() // добавлено логирование
+				defer func() {
+					if cw.statusCode >= http.StatusOK && cw.statusCode < http.StatusMultipleChoices {
+						if err := cw.Close(); err != nil {
+							sugar.Errorw("Failed to close compress writer", err)
+						}
+						cw.zw.Reset(cw.w)
+					}
+				}()
 				sugar.Infow("Compression applied", "encoding", "gzip")
 			}
 
-			// проверка отправки gzip от клиента
 			contentEncoding := r.Header.Get("Content-Encoding")
 			sendsGzip := strings.Contains(contentEncoding, "gzip")
 			if sendsGzip {
-				// обертка для разжатия
 				cr, err := newCompressReader(r.Body)
 				if err != nil {
 					w.WriteHeader(http.StatusInternalServerError)
@@ -104,7 +123,6 @@ func WithCompression(sugar *zap.SugaredLogger) func(http.Handler) http.Handler {
 				sugar.Infow("Decompression applied", "encoding", "gzip")
 			}
 
-			// передача управления следующему обработчику
 			next.ServeHTTP(ow, r)
 		})
 	}
